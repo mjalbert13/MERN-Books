252f2bd8f55c4a28355c21e528eb6cfe8b4110ee
mjalbert13 <mjalbert13@gmail.com>
2019-08-13 13:06:06 -0400

    react rendering

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,19 @@
 import React, { Component } from "react";
+import Saved from './Pages/Saved/Saved';
+import Search from './Pages/Search/Search';
 import Navbar from './Components/Navbar/Navbar'
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
-import Search from './Pages/Search/Search'
-import Saved from './Pages/Saved/Saved'
+
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div>
-        <Navbar />
-          <Route exact path="/search" component={Search} />
-          <Route exact path="/saved" component={Saved} />
-        </div>      
-      </Router>
-     
+     <div>
+       <Navbar />
+       <Saved />
+     </div>
+
+    
     );
   }
 }
